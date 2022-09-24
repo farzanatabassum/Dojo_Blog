2b54a4df0fa2210c3f826996731ee8c76151266b
farzanatabassum <farzanatabassum531@gmail.com>
2022-09-24 23:27:50 +0600

    event

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,16 @@
 const Home = () => {
+    const handleClick=(e)=>{
+        console.log('hello',e);
+    }
+    const handleClickTwice=(name,e)=>{
+        console.log('hello '+ name, e.target);
+    }
+
     return ( 
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e)=>handleClickTwice('farzana',e)}>Click twice</button>
         </div>
      );
 }
